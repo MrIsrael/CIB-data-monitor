6e2447879e79e80af4e3f576651cb7984f625dd4
israel Uribe <israeluribe@gmail.com>
2020-08-19 15:22:41 -0500

    API fetching working properly. Loop mode enabled.

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
-// Cálculo CIB en pesos en tiempo real = ( CIB * (USD/COP) ) / 4
-const interval = 1000                             // valor en milisegundos
-let timeSinceLastChange = 0, lastValue = 0
+// Real time CIB value in COP calculation = ( CIB * (USD/COP) ) / 4
+// GET call: http://localhost:3500/api/data
 
+let secondsSinceLastChange = 0, lastValue = 0
+const interval = 1000                                             // value in milliseconds
+const realTimeValue = document.getElementById('calculation')
+
+// Time since last sucessful data fetch
 let timer = setInterval(() => {
-  let random_val = ((Math.random() * 3600) + 1)
-
-  if (random_val !== lastValue && random_val > 2500) {
-    document.getElementById('calculation').setAttribute('class', (random_val > lastValue ? 'bigger' : 'lower'))
-    document.getElementById('calculation').innerHTML = '$' + random_val.toFixed(2)
-    console.log('Nuevo: ' + random_val.toFixed(2) + ', Anterior: ' + lastValue.toFixed(2) + (random_val > lastValue ? ', NUEVO ES MAYOR, VERDE' : ', NUEVO ES MENOR, ROJO'))
-    timeSinceLastChange = 0
-    lastValue = random_val
-  }
-
-  console.log('Nuevo: ' + random_val.toFixed(2))
-  document.getElementById('last-value-change').innerHTML = 'Último cambio de valor hace: ' + timeSinceLastChange + ' segundos'
-  timeSinceLastChange++
+  document.getElementById('last-value-change').innerHTML = 'Último cambio de valor hace: ' + secondsSinceLastChange + ' segundos'
+  secondsSinceLastChange++
 }, interval)
+
+// Get data from external API
+const scrapeData = async () => {
+  await fetch('http://localhost:3500/api/data/')
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          const exchangeRate = parseInt(data[0].usd_cop)
+          const stockValue = parseFloat(data[1].nyse_cib)
+          const cibRealTime = ((stockValue * exchangeRate) / 4)
+
+          if (cibRealTime !== lastValue) { 
+            realTimeValue.setAttribute('class', (cibRealTime > lastValue ? 'bigger' : 'lower')) 
+          } else {
+            realTimeValue.setAttribute('class', 'same') 
+          }
+
+          realTimeValue.innerHTML = '$' + cibRealTime.toFixed(2)
+          document.getElementById('usd_cop').innerHTML = 'Último valor usd_cop consultado: COP$' + exchangeRate
+          document.getElementById('nyse_cib').innerHTML = 'Último valor nyse_cib consultado: USD$' + stockValue
+
+          console.log('CIB en pesos anterior: ' + lastValue.toFixed(2))
+          secondsSinceLastChange = 0
+          lastValue = cibRealTime
+
+          // return data
+        })
+        .catch(err => console.log(err))
+  setTimeout(scrapeData, 1000)                                  // keep running API fetch function over and over, calling itself when done
+}
+
+scrapeData()
