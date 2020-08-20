4b860f39a9262f312cfb14a116afc5301dac43e6
israel Uribe <israeluribe@gmail.com>
2020-08-20 16:13:37 -0500

    Layout enhancements added.

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // Real time CIB value in COP calculation = ( CIB * (USD/COP) ) / 4
-// GET call: http://localhost:3500/api/data
+// Endpoint (GET): http://localhost:3500/api/data
 
 let secondsSinceLastChange = 0, secondsSinceLastDataFetch = 0, lastValue = 0, showData = true
 const interval = 1000                                             // value in milliseconds
 const realTimeValue = document.getElementById('calculation')
 
-// Time since last sucessful data fetch
+// Time measurement
 let timer = setInterval(() => {
   document.getElementById('last-value-change').innerHTML = 'Último cambio de valor hace: ' + 
-      (secondsSinceLastChange >= 60 ? (Math.floor(secondsSinceLastChange / 60) + ' minutos, ' + (secondsSinceLastChange % 60) + ' segundos')
-      : secondsSinceLastChange + ' segundos')
-  document.getElementById('last-data-fetch').innerHTML = 'Última consulta de datos hace: ' + secondsSinceLastDataFetch + ' segundos'
+      (secondsSinceLastChange >= 60 ? (parseInt(secondsSinceLastChange / 60) + 
+      (secondsSinceLastChange >= 120 ? ' minutos, ' : ' minuto, ') + (secondsSinceLastChange % 60) + 
+      (secondsSinceLastChange % 60 === 1 ? ' segundo' : ' segundos'))
+      : (secondsSinceLastChange + (secondsSinceLastChange === 1 ? ' segundo' : ' segundos')))
+  document.getElementById('last-data-fetch').innerHTML = 'Última consulta de datos hace: ' + 
+      secondsSinceLastDataFetch + (secondsSinceLastDataFetch === 1 ? ' segundo' : ' segundos')
   secondsSinceLastChange++
   secondsSinceLastDataFetch++
 }, interval)
@@ -28,7 +31,7 @@ function hideShowInfo() {
   document.getElementById('btn').innerHTML = buttonText
 }
 
-// Get data from external API
+// Get data from external API, constantly
 const scrapeData = async () => {
   await fetch('http://localhost:3500/api/data/')
         .then(res => res.json())
@@ -38,23 +41,34 @@ const scrapeData = async () => {
           const stockValue = parseFloat(data[1].nyse_cib)
           const cibRealTime = ((stockValue * exchangeRate) / 4)
 
-          if (cibRealTime !== lastValue) { 
-            realTimeValue.setAttribute('class', (cibRealTime > lastValue ? 'bigger' : 'lower')) 
-            secondsSinceLastChange = 0
+          // Si los datos se reciben correctamente, isNaN(cibRealTime) = false
+          if ( !isNaN(cibRealTime) ) {
+
+            if (cibRealTime !== lastValue) { 
+              realTimeValue.setAttribute('class', (cibRealTime > lastValue ? 'bigger' : 'lower')) 
+              secondsSinceLastChange = 0
+            } else {
+              realTimeValue.setAttribute('class', 'same') 
+            }
+  
+            realTimeValue.innerHTML = '$' + parseInt(cibRealTime / 1000) + '.' + parseInt(cibRealTime % 1000)
+            console.log('CIB en pesos anterior: ' + lastValue.toFixed(2))
+            lastValue = cibRealTime
+
+          // isNaN(cibRealTime) = true --> La API no devolvió algún dato en el formato requerido, o retornó null
           } else {
-            realTimeValue.setAttribute('class', 'same') 
+            realTimeValue.innerHTML = 'Recepción de dato no válido...'
           }
 
-          realTimeValue.innerHTML = '$' + parseInt(cibRealTime / 1000) + '.' + parseInt(cibRealTime % 1000)
-          secondsSinceLastDataFetch = 0
           document.getElementById('usd_cop').innerHTML = 'Último valor usd_cop consultado: COP$' + exchangeRate
           document.getElementById('nyse_cib').innerHTML = 'Último valor nyse_cib consultado: USD$' + stockValue
-
-          console.log('CIB en pesos anterior: ' + lastValue.toFixed(2))
-          lastValue = cibRealTime
+          secondsSinceLastDataFetch = 0          
         })
-        .catch(err => console.log(err))
-  setTimeout(scrapeData, 1000)                                  // keep running API fetch function over and over, calling itself when done
+        .catch(err => {
+          realTimeValue.innerHTML = 'Sin respuesta desde API. Está encendido CIB-data-provider?'
+          console.log(err)
+        })
+  setTimeout(scrapeData, 1000)                                    // keep running API fetch function over and over, calling itself when done
 }
 
 scrapeData()
